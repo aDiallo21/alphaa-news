9b64518d1ce37cb456d7c352033d79f0549d874b
adiallo21 <abdoulaye32397@gmail.com>
2022-09-27 15:27:10 +0100

    add route to topic component

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/articles" element={<Articles />} />
           <Route path="/" element={<Home />} />
+          <Route path="/topics" element={<Topics />} />
         </Routes>
       </div>
     </BrowserRouter>
