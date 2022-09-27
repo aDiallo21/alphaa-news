fcb4f72dd31635b06b398fb3fac4f66868bb612b
adiallo21 <abdoulaye32397@gmail.com>
2022-09-27 15:20:52 +0100

    add comment count and likes to every article

--- src/components/ArticleCard/ArticleCard.jsx
+++ src/components/ArticleCard/ArticleCard.jsx
@@ -6,7 +6,10 @@ function ArticleCard({ article }) {
       <h3> {article.title}</h3>
       <p>Topic: {article.topic}</p>
       <p>Author: {article.author}</p>
+      <p> {article.created_at}</p>
       <p> {article.body}</p>
+      <p> Likes: {article.votes}</p>
+      <p> Comments: {article.comment_count}</p>
     </div>
   );
 }
