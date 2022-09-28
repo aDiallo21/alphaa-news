0f7b1fee41d96088ee97719bed1f3d39e466961d
adiallo21 <abdoulaye32397@gmail.com>
2022-09-28 15:32:39 +0100

    add list of all topics

---
+++ src/components/Topics/Topics.jsx
@@ -0,0 +1,38 @@
+import { getTopics } from "../../utils/axios";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+export default function Topics() {
+  const [topics, setTopics] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    getTopics().then((topics) => {
+      setTopics(topics);
+      setLoading(false);
+    });
+  }, []);
+
+  return (
+    <main>
+      <div>
+        <p className="topic-msg">Click each topic to filter the articles</p>
+        {loading ? (
+          <h1>"Loading... please wait"</h1>
+        ) : (
+          topics.map((topic) => {
+            return (
+              <p>
+                <Link className="link" to={`/articles/${topic.slug}`}>
+                  <h3>
+                    {topic.slug[0].toUpperCase() + topic.slug.substring(1)}
+                  </h3>
+                </Link>
+                {topic.description}
+              </p>
+            );
+          })
+        )}
+      </div>
+    </main>
+  );
+}
