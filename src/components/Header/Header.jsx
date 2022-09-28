2b5f32b137539095a4b66488f976cc61cfd759d9
adiallo21 <abdoulaye32397@gmail.com>
2022-09-28 15:40:01 +0100

    add homepage links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,16 +5,15 @@ const Header = () => {
   return (
     <nav>
       <Link to="/">
-        {" "}
-        <h1 className="header">Alphaa News</h1>{" "}
+        <h1 className="header">Alphaa News</h1>
       </Link>
       <Link className="link" to="/articles">
         Articles
       </Link>
-      <Link className="link" to="/articles">
+      <Link className="link" to="/topics">
         Topics
       </Link>
-      <Link className="link" to="/articles">
+      <Link className="link" to="/users">
         Users
       </Link>
     </nav>
