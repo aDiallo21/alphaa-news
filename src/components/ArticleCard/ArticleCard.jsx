09238de3a55dcb87d95ea105eec7ec4f4df0ba19
adiallo21 <abdoulaye32397@gmail.com>
2022-09-29 14:57:58 +0100

    modify the article card

--- src/components/ArticleCard/ArticleCard.jsx
+++ src/components/ArticleCard/ArticleCard.jsx
@@ -2,16 +2,18 @@ import { Link } from "react-router-dom";
 function ArticleCard({ article }) {
   return (
     <div className="article-card">
-      <h3> {article.title}</h3>
+      <Link className="link" to={`/articles/${article.article_id}`}>
+        <h3> {article.title}</h3>
+      </Link>
       <p>
         Topic:
         <Link className="link" to={`/articles/topics/${article.topic}`}>
           {article.topic}
         </Link>
       </p>
-      <p>Author: {article.author}</p>
-      <p> {article.created_at}</p>
-      <p> Likes: {article.votes}</p>
+      <p> Author: {article.author}</p>
+      <p> Publish date: {article.created_at}</p>
+      <p> Votes: {article.votes}</p>
       <p> Comments: {article.comment_count}</p>
     </div>
   );
