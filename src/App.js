fd993f3ca5a48be5467a530f3aa73f0eb83589f8
adiallo21 <abdoulaye32397@gmail.com>
2022-09-29 14:57:39 +0100

    add path to article id

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Articles from "./components/Articles/Articles";
 import Topics from "./components/Topics/Topics";
+import SingleArticle from "./components/SingleArticle/SingleArticle";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/articles" element={<Articles />} />
           <Route path="/articles/topics/:topic_slug" element={<Articles />} />
           <Route path="/topics" element={<Topics />} />
+          <Route path="/articles/:article_id" element={<SingleArticle />} />
         </Routes>
       </div>
     </BrowserRouter>
