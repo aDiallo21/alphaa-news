8af2699ea2973f9cd14854fe7212608d76692602
adiallo21 <abdoulaye32397@gmail.com>
2022-09-30 15:51:49 +0100

    add route to comments

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import Articles from "./components/Articles";
 import Topics from "./components/Topics";
 import SingleArticle from "./components/SingleArticle";
+import { Comments } from "./components/Comments";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/articles/topics/:topic_slug" element={<Articles />} />
           <Route path="/topics" element={<Topics />} />
           <Route path="/articles/:article_id" element={<SingleArticle />} />
+          <Route path="/articles/:article_id/comments" element={<Comments />} />
         </Routes>
       </div>
     </BrowserRouter>
